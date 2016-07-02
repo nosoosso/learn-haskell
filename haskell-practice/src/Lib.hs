acd541871fb3db58487fdd19a699ca9a655f3841
nosoosso <nosoosso.null@gmail.com>
2016-07-02 20:12:42 +0900

    add Either

--- haskell-practice/src/Lib.hs
+++ haskell-practice/src/Lib.hs
@@ -1,9 +1,25 @@
-module Lib
-    ( Either'(..)
-    ) where
+module Lib where
 
-data Either' a b = Left' a | Right' b
+-- right-bias Either
+data Either' a b = Left' a | Right' b deriving (Eq, Ord, Read, Show)
+instance Functor (Either' a) where
+  fmap _ (Left' x) = Left' x
+  fmap f (Right' x) = Right' $ f x
 
--- instance Functor (Either' a) where
---   fmap f (Left' x) = f x
---   fmap f (Right' x) = Right' x
+-- left-bias Either
+data Either'' b a = Right'' b | Left'' a  deriving (Eq, Ord, Read, Show)
+instance Functor (Either'' a) where
+  fmap f (Left'' x) = Left'' $ f x
+  fmap _ (Right'' x) = Right'' x
+
+-- s = show $ Left' 3
+--
+-- r :: IO ()
+-- r = do
+--   return $ show $ Left' 3
+--   -- print (Right' 3)
+--   print $ fmap (+ 1) (Left' 3)
+--   -- print $ fmap (+ 1) (Right' 3)
+--   -- print $ fmap (+ 1) (Left'' 3)
+--   print $ fmap (+ 1) (Right'' 3)
+--   return ()
